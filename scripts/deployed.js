f40dee9c7f2f3df796351cc61f04d0348c69daaa
opcc <opcc22059@gmail.com>
2022-03-23 20:59:39 +0800

    update

---
+++ scripts/deployed.js
@@ -0,0 +1,12 @@
+const contracts = {
+	// tokens and contracts
+    izumiTest: {
+        iZi: "0xEe5e3852434eB67F8e9E97015e32845861ea15E8",
+        HOURAI: '0x972055129959Ddf875d29bab013d5685eb251811',
+    },
+    ethereum: {
+	    iZi: "0x9ad37205d608B8b219e6a2573f922094CEc5c200",
+    }
+}
+
+module.exports = contracts;
